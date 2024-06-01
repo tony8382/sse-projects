fb692f224244ba1ee18560b5c1dc4e05e300f2ea
Li-Yu, Yang <tony830802@gmail.com>
2024-06-01 21:05:59 +0800

    add table

---
+++ Frontend/src/app/table/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Table',
+}
+export default function Page() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      GGGG
+    </div>
+  );
+}
